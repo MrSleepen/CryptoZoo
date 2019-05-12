85ee3b8dbdc796ab4f0045d4c9b3c1e670011cdb
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2019-05-12 20:29:47 -0300

    Added save  State to the game

---
+++ CryptoZoo/Assets/Scripts/Hunger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunger : MonoBehaviour {
+
+    public bool JustBorn = false;
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Debug.Log(PlayerPrefs.GetInt("HungerLevel"));
+        
+        if (JustBorn == true)
+        {
+            PlayerPrefs.SetInt("HungerLevel", 100);
+        }
+
+        while(PlayerPrefs.GetInt("HungerLevel")>=0){
+            StartCoroutine(GettingHungry());
+        }
+	}
+
+    public IEnumerator GettingHungry()
+    {
+        int HungerLevel = PlayerPrefs.GetInt("HungerLevel");
+        PlayerPrefs.SetInt("HungerLevel", HungerLevel -=1);
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ CryptoZoo/Assets/Scripts/SaveScripts/SaveState.cs
@@ -0,0 +1,9 @@
+﻿
+
+public class SaveState
+{
+
+    public int HungerLevel;
+   
+ 
+}
