2db4ce3002f88b443535661b0ee5a548492139c2
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2019-06-24 19:31:25 -0300

    Monster Spawner added, with BASIC monster number system

--- CryptoZoo/Assets/Scripp.cs
+++ CryptoZoo/Assets/Scripp.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class Scripp : MonoBehaviour {
 
-    public GameObject Monsters;
+    public GameObject Monster1;
+    public GameObject Monster2;
+    public int MonsterNumber  = 1;
     // Use this for initialization
+
+        void Update()
+    {
+        Debug.Log("dasdas");
+    }
+
+
     public void Spawn()
     {
-        Instantiate(Monsters);
+        if (MonsterNumber == 1)
+        {
+           Instantiate(Monster1);
+        }
+        
+
+        if(MonsterNumber == 2 )
+        {
+            Instantiate(Monster2);
+        }
+            
     }
-}
+    public void ChangeMonster()
+    {
+        if(MonsterNumber == 1)
+        {
+            MonsterNumber = 2;
+        }
+        else
+        {
+            MonsterNumber = 1;
+        }
+    }
+    }
