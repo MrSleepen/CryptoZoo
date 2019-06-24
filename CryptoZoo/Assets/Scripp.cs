49e6d5d85e8ebab7d810aebc979025aefea0c5d8
MrSleepen <43214874+MrSleepen@users.noreply.github.com>
2019-06-24 19:11:29 -0300

    Spawner

---
+++ CryptoZoo/Assets/Scripp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripp : MonoBehaviour {
+
+    public GameObject Monsters;
+    // Use this for initialization
+    public void Spawn()
+    {
+        Instantiate(Monsters);
+    }
+}
